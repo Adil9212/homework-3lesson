bb6d0c645681c5a3c1e707e102e80ca84242a891
Adil9212 <abdrazakovadil@gmail.com>
2023-05-04 18:11:28 +0600

    lesson-3

---
+++ script.js
@@ -0,0 +1,59 @@
+   // h \ w 1
+   // function findSmallerNumber(num1, num2) {
+   //     if (num1 < num2) {
+   //       return num1;
+   //     } else {
+   //       return num2;
+   //     }
+   //   }
+   //   console.log(findSmallerNumber(10, 5)); // Выдаёт: 5
+   // console.log(findSmallerNumber(20, 30)); // Выдаёт: 20
+
+
+   //h \ w - 2
+   // function power(base, exponent) {
+   //     let result = 1;
+
+   //     for (let i = 0; i < exponent; i++) {
+   //       result *= base;
+   //     }
+
+   //     return result;
+   //   }
+   //   console.log(power );
+
+
+   // h \ w - 3
+   // function sumArray(arry) {
+   //     let sum = 0;
+   //     for (let i = 0; i < arry.length; i++) {
+   //         sum += arry[i];
+   //     }
+   //     return sum;
+   // }
+   // const sum = [1, 2, 3, 4, 5];
+   // console.log(sumArray(sum));
+
+   // h \ w 4
+   // const user = {
+   //     phone_number: "+992204030",
+   //     balance: 20000,
+   //     sendTransfer: (phoneNumber, sum) => {
+   //         console.log('Номер телефона:', phoneNumber, 'сумма для перевода:', sum, 'остаток',
+   //             user.balance - sum);
+   //     }
+
+   // }
+   // console.log(user.sendTransfer('+996500500500', 6000));
+
+   //h \ w 5
+   //    const bankAccount = {
+   //        accountNumber: '+996995200910',
+   //        balance: 5000,
+   //        sendTransfer: (Bank_account_number, amount) => {
+   //            console.log('номер банковского счета:', Bank_account_number, 'сумма для перевода', amount, 'остаток', bankAccount.balance - amount);
+   //        }
+   //    }
+   //    console.log(bankAccount.sendTransfer('+996995200910', 2000))
+   //    bankAccount.balance = 7000
+   //    console.log(bankAccount);
